68e73b8a670cb17cc7bb06cc6a3a71c5f8fc3872
Diogo Vasques <49960268+DiVasques@users.noreply.github.com>
2023-10-23 07:12:03 -0300

    feat: implemented base project structure (#1)

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:gami_acad_web/ui/routers/generic_router.dart';
+import 'package:intl/date_symbol_data_local.dart';
 
+final globalNavigatorKey = GlobalKey<NavigatorState>();
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  initializeDateFormatting('pt_BR');
   runApp(const MainApp());
 }
 
 class MainApp extends StatelessWidget {
+  final Color primaryColor = const Color(0xFF1F5F02);
   const MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return MaterialApp(
+      title: 'GamiAcad',
+      navigatorKey: globalNavigatorKey,
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        appBarTheme: AppBarTheme(
+          iconTheme: IconThemeData(color: primaryColor),
+          elevation: 0,
+          backgroundColor: Colors.transparent,
+          centerTitle: true,
         ),
+        bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+        ),
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: primaryColor,
+        ),
+        dividerTheme: const DividerThemeData(
+          space: 2,
+          color: Colors.black54,
+        ),
+        floatingActionButtonTheme: FloatingActionButtonThemeData(
+          backgroundColor: primaryColor,
+          elevation: 2,
+        ),
+        fontFamily: 'Montserrat',
+        primaryColor: primaryColor,
+        scaffoldBackgroundColor: Colors.white,
       ),
+      onGenerateRoute: GenericRouter.generateRoute,
+      initialRoute: GenericRouter.loginRoute,
     );
   }
 }
