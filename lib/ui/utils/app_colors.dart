55f5be4580bc59fe6aaee1daf95c8467388123f6
Diogo Vasques <49960268+DiVasques@users.noreply.github.com>
2023-10-27 07:36:36 -0300

    feat: home drawer selected color now matches background (#8)

--- lib/ui/utils/app_colors.dart
+++ lib/ui/utils/app_colors.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   static const Color primaryColor = Color(0xFF1F5F02);
+  static const Color backgroundColor = Colors.white;
   static const Color darkerPrimaryColor = Color.fromARGB(255, 21, 63, 2);
   static const Color lighterPrimaryColor = Color.fromARGB(255, 61, 178, 7);
 
